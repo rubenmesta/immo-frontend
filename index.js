2ea7226a8519932cd3c6673a333831df707b374b
Ruben Mesta <rubenmesta29@gmail.com>
2021-05-05 11:00:46 -0400

    added cors middleware

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express');
-const path = require("path");
+// const bodyParser = require('body-parser');
 
 const app = express();
 
+const helmet = require('helmet') 
+const cors = require('cors') 
+
 app.use(express.json()); 
+// app.use(bodyParser.json());
+
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://harvard-capstone.herokuapp.com/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(helmet())
+// --> Add this
+app.use(cors(corsOptions))
+
 
 const PORT = process.env.PORT || 5000;
 
 require('./routes/dialogFlowRoutes')(app);
+
 app.get('/', (req, res) => {
-  res.send({'hello': 'Index'});
+    res.send({'hello': 'From Index'});
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is up at port: ${PORT}`)
